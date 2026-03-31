4a887ae30bc907a707a786ea7ca66abba37a301e
Bojan Vrhovnik <bojan@vrhovnik.net>
2026-03-31 11:27:48 +0200

    Initialize project structure with .gitignore, solution file, and core functionality for tax calculation

---
+++ src/ADMcpExamples/MCP.DotNetMethod/DotnetMethodHelper.cs
@@ -0,0 +1,20 @@
+﻿using Spectre.Console;
+
+namespace MCP.DotNetMethod;
+
+public class DotnetMethodHelper
+{
+    public static string CalculateTax(string customer, int forMonths)
+    {
+        AnsiConsole.WriteLine($"Calculating tax for customer {customer} for past {forMonths} months.");
+        var multiplier = 2;
+        var result = customer switch
+        {
+            "Method" => forMonths * multiplier,
+            "Tax" => (double)forMonths / multiplier,
+            _ => forMonths
+        };
+        var tax = $"Tax for customer {customer}  for past {forMonths} months is {result}";
+        return tax;
+    }
+}
---
+++ src/ADMcpExamples/MCP.DotNetMethod/Program.cs
@@ -0,0 +1,51 @@
+﻿using Azure.AI.OpenAI;
+using Azure.Identity;
+using MCP.DotNetMethod;
+using Microsoft.Extensions.AI;
+using Spectre.Console;
+
+AnsiConsole.MarkupLine("[blue]Using .NET method as ta tool[/]");
+
+#region Environment variables
+
+var endpoint = Environment.GetEnvironmentVariable("Endpoint");
+ArgumentException.ThrowIfNullOrEmpty(endpoint, "Endpoint environment variable is not set.");
+var deploymentName = Environment.GetEnvironmentVariable("DeploymentName");
+ArgumentException.ThrowIfNullOrEmpty(deploymentName, "DeploymentName environment variable is not set.");
+
+#endregion
+
+var credentials = new DefaultAzureCredential();
+IChatClient client =
+    new ChatClientBuilder(
+            new AzureOpenAIClient(new Uri(endpoint), credentials)
+                .GetChatClient(deploymentName)
+                .AsIChatClient())
+        .UseFunctionInvocation()
+        .Build();
+
+var chatOptions = new ChatOptions
+{
+    Tools =
+    [
+        AIFunctionFactory.Create(DotnetMethodHelper.CalculateTax,
+            "get_tax_for_customer",
+            "Gets tax for customer for specific period of month")
+    ]
+};
+
+List<ChatMessage> chatHistory =
+[
+
+    new(ChatRole.System, """
+                         You are friendly business tax consultant. 
+                         You calculate tax for specific period of months.
+                         """),
+    new(ChatRole.User,
+        "I am your customer Method. What's is my tax information for past 3 months?")
+];
+
+AnsiConsole.MarkupLine($"[gray]{chatHistory.Last().Role} >>> {chatHistory.Last()}[/]");
+
+var response = await client.GetResponseAsync(chatHistory, chatOptions);
+AnsiConsole.MarkupLine($"[red]Assistant[/] >>> {response.Text}");
